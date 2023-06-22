81eb5adb16e76134f151bbed30d4f5cced3bf751
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-06-21 21:11:02 -0500

    =

--- cephrius-connect/screens/MeetingRoom.js
+++ cephrius-connect/screens/MeetingRoom.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { View,Text,StyleSheet,TextInput } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import StartMetting from '../components/StartMeeting'
 
 
 
@@ -14,41 +15,13 @@ function MeetingRoom() {
   return (
         <View style={styles.container}>
             {/* Start Meeting section */}
-            <View style={styles.startMeetingContainer}>
-                <View style = {styles.info}>
-                    <TextInput
-                        style = {styles.TextInput}
-                        value={name}
-                        placeholder='Enter name'
-                        placeholderTextColor="#767476"
-                        onChangeText = {text => setName(text)}
-                    />
-                </View> 
-
-                <View style={styles.info}>
-                    <TextInput
-                        style = {styles.TextInput}
-                        value={roomId}
-                        placeholder='Enter Room ID'
-                        placeholderTextColor="#767476"
-                        onChangeText = {text => setRoomID(text)}
-                    />
-                </View>  
-                <View style ={{ alignItems: "center" }}>
-                    <TouchableOpacity
-                    onPress = {() =>{}}
-                    style = {styles.startMeetingButton}
-                    >
-                    
-                            <Text style = {{color: "white", fontWeight: "bold"}}>
-                                Start Meeting
-                            </Text>
-
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <StartMeeting 
+                name = {name}
+                setName ={setName}
+                roomId = {roomId}
+                setRoomID = {setRoomID}
+            />
         </View>
-    
   )
 }
 
@@ -58,32 +31,6 @@ const styles = StyleSheet.create({
     container:{
         backgroundColor: "#1c1c1c",
         flex: 1,
-
-
-    },
-    info: {
-        width: "100%",
-        backgroundColor: "#373538",
-        height: 50,
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: "#484648",
-        padding: 12,
-        justifyContent: "center",
-
-
-    },
-    TextInput: {
-        color: "white",
-        fontSize: 19
-    },
-    startMeetingButton: {
-        width: 350,
-        marginTop: 20,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#344feb",
-        height: 50,
-        borderRadius: 15
     }
+
 })
--- zoom-clone-api/index.js
+++ zoom-clone-api/index.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const { SocketAddress } = require("net");
 const app = express();
 const server = require("http").Server(app)
 const io = require("socket.io")(server)
@@ -9,7 +10,16 @@ app.get("/", (req, res) => {
     res.send("Hellow World")
 })
 
+io.on("connected", socket => {
+    console.log("Someone is connected")
+    socket.on("join-room", ({ roomId, userName })=>{
+        console.log("User Joined Room")
+        console.log(roomId);
+        console.log(userName)
+    })
+})
 
 server.listen(port, ()=> {
     console.log('Zoom clone API listing on localhost:3001')
 })
+
