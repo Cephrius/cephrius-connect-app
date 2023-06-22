23b56f8d61b05dc4bb91fcac144d9fb6a6267e62
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-06-21 22:50:56 -0500

    -

--- cephrius-connect/screens/MeetingRoom.js
+++ cephrius-connect/screens/MeetingRoom.js
@@ -1,17 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { View,Text,StyleSheet,TextInput } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import StartMetting from '../components/StartMeeting'
+import StartMeeting from '../components/StartMeeting'
+import { io } from "socket.io-client"
 
-
+let socket;
 
 function MeetingRoom() {
 
 
   const [name, setName] = useState()
   const [roomId, setRoomID] = useState()
-  
-  
+
+  useEffect(() =>{
+        const API_URL = " https://5687-2601-2c1-8083-3a00-f56c-35ac-a4b8-5298.ngrok-free.app "
+        socket = io('${API_URL}');
+        console.log("Hello")
+        socket.on('connection', () =>console.log("connected"))
+        
+  }, [])
+//   skips the browser warning screen
+  fetch(' https://5687-2601-2c1-8083-3a00-f56c-35ac-a4b8-5298.ngrok-free.app', {
+            headers: {
+        'ngrok-skip-browser-warning': 'true' // Set the header with the desired value
+  }
+})
   return (
         <View style={styles.container}>
             {/* Start Meeting section */}
